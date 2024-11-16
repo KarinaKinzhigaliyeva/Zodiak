2cb5a3af86e83b973e791d6097984ebd758bdbc5
Karina Kinzhigaliyeva <karinakinzhigaliyeva@gmail.com>
2024-11-16 20:07:04 +0500

    Initial Commit

---
+++ zodiac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zodiac
+//
+//  Created by Karina Kinzhigaliyeva on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ zodiacTests/zodiacTests.swift
@@ -0,0 +1,17 @@
+//
+//  zodiacTests.swift
+//  zodiacTests
+//
+//  Created by Karina Kinzhigaliyeva on 16.11.2024.
+//
+
+import Testing
+@testable import zodiac
+
+struct zodiacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
